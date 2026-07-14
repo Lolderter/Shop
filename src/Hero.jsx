5988ab329558423238dbb1095060e64db098711b
Lolderter <namtimur813@gmail.com>
2026-07-14 10:19:56 +0100

    ternary operator on buy button

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -14,42 +14,48 @@ function Hero({ headline, description }) {
       name: "Laptop",
       brand: "Dell",
       category: "Electronics",
-      price: "800$"
+      price: "800$",
+      count:1
     },
     {
       id: 2,
       name: "Phone",
       brand: "Samsung",
       category: "Electronics",
-      price: "250$"
+      price: "250$",
+      count:1
     },
     {
       id: 4,
       name: "iPad",
       brand: "Apple",
       category: "Electronics",
-      price: "500$"
+      price: "500$",
+      count:0
     },
     {
       id: 5,
       name: "Laptop",
       brand: "Lenovo",
       category: "Electronics",
-      price: "1000$"
+      price: "1000$",
+      count:1
     },
     {
       id: 6,
       name: "Microphone",
       brand: "Blue Yeti",
       category: "Accessories",
-      price: "200$"
+      price: "200$",
+      count:1
     },
     {
       id: 7,
       name: "Headphones",
       brand: "Sony",
       category: "Accessories",
-      price: "300$"
+      price: "300$",
+      count:1
     },
   ];
 
@@ -58,6 +64,7 @@ function Hero({ headline, description }) {
   const filteredProducts = products
     .filter((p) => selectedCategory === "All" || p.category === selectedCategory)
     .filter((p) => p.name.toLowerCase().includes(query.toLowerCase()));
+   
 
   return (
     <div className="hero">
@@ -95,10 +102,15 @@ function Hero({ headline, description }) {
             <h3 className="product-name">{product.name}</h3>
             <p className="product-brand">Brand: {product.brand}</p>
             <p className="product-category">Category: {product.category}</p>
+            <p className="product-category"> Count:{product.count}</p>
             <p className="product-price">{product.price}</p>
+            
             <p className="product-price">
-              <button onClick={handleClick}>Buy Now</button>
+              <button disabled={product.count === 0} onClick={handleClick}>
+                {product.count === 0 ? "Out of Stock" : "Buy Now"}
+              </button>
             </p>
+            
           </div>
         ))}
       </div>
